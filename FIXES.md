8c6668bf204839bf7ca5a3fbc5c7dce5c739ada2
Nathaniel Watts <reg@nathanielwatts.com>
2013-11-01 14:03:51 -0600

    first blogger commit

---
+++ FIXES.md
@@ -0,0 +1,19 @@
+# 40 - subl command
+
+# 48 "as is the" - "which is the"
+
+# 53 "test::unit" ? --> minitest?
+
+# 61 - which will happen automatically, the database will be stored *as*, etc.
+#
+# 98 'bin/rails' ??
+#
+# 115 migration code - https://www.evernote.com/shard/s5/sh/da674e28-ff99-4706-829a-0860e4efc8af/4abf330d4494e26e10c12c9530cb3e6d
+#
+# 125 **everything&&
+#
+# "- so far an empty array.", which is empty right now because we haven't saved any articles to the database yet.
+#
+# "the answer is a technique called reflection" --> *reflection* => make it italicsjA
+#
+# "relative path verbs" http://www.coffeecup.com/help/articles/absolute-vs-relative-pathslinks/
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blogger::Application.config.secret_key_base = '102f5046400c318f8f04465a9a680b476675cb53eee5477ab1eb4cd721b812a049be960cee4efc2c1d9345a73d32101abe02f468a4fa0b2d25bae33958b760a6'
