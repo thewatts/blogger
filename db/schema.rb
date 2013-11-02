f3eb67cd85b2a47b7f1f283fec2f945c6af14250
Nathaniel Watts <reg@nathanielwatts.com>
2013-11-01 19:02:48 -0600

    add attachments and images to article - needing to remove images from article

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131101211502) do
+ActiveRecord::Schema.define(version: 20131102005051) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
     t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
+  end
+
+  create_table "attachments", force: true do |t|
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
+    t.integer  "article_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
